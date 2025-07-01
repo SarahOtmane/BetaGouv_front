d7a3ffda059080be1eb6f1657da2aef41a4f6aa6
amiramiour <amiouramirtahar@gmail.com>
2025-07-01 14:44:08 +0200

    add CalendrierStages component

---
+++ app/src/components/actions/CalendrierStages.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CalendrierStages: React.FC = () => {
+  return (
+    <div>
+      <h4 className="fr-text--md fr-mb-2w">Calendrier des stages</h4>
+      <p className="fr-text--sm">Stage 3ème : du 3 mars au 2 avril 2024</p>
+    </div>
+  );
+};
+
+export default CalendrierStages;
