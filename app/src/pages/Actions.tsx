56b7c86e91a60cfc29bda95076bb62402435fc5a
amiramiour <amiouramirtahar@gmail.com>
2025-07-01 15:06:17 +0200

    add actions page

--- app/src/pages/Actions.tsx
+++ app/src/pages/Actions.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import PageTitle from '../components/installations/PageTitle'; 
+import PageTitle from '../components/installations/PageTitle';
 import FilAriane from '../components/metierscomponents/FilAriane';
+import HeaderMenu from '../components/metierscomponents/HeaderMenu';
 import ActionsChecklist from '../components/actions/ActionsChecklist';
 import ActionsProgrammes from '../components/actions/ActionsProgrammes';
 import CalendrierStages from '../components/actions/CalendrierStages';
@@ -8,11 +9,21 @@ import CalendrierStages from '../components/actions/CalendrierStages';
 const Actions: React.FC = () => {
   return (
     <div className="fr-container fr-mt-5w">
-        <PageTitle />
-      <FilAriane items={[
-        { label: 'Mon lycée', href: '#' },
-        { label: 'Les formats proposés par le lycée' }
-      ]} />
+      <div className="fr-grid-row fr-grid-row--between fr-mb-3w">
+        <div className="fr-col-12 fr-col-md-6">
+          <h1 className="fr-h2">Je valorise mon lycée</h1>
+        </div>
+        <div className="fr-col-12 fr-col-md-6 fr-text--right">
+          <HeaderMenu />
+        </div>
+      </div>
+
+      <FilAriane
+        items={[
+          { label: 'Mon Lycée', href: '#' },
+          { label: 'Les formats proposés par le lycée' },
+        ]}
+      />
 
       <div className="fr-grid-row fr-grid-row--gutters fr-mt-5w">
         <div className="fr-col-12 fr-col-md-6">
