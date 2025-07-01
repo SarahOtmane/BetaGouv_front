7892eae5f17d786d87372e97254161b8c957a2c1
amiramiour <amiouramirtahar@gmail.com>
2025-07-01 15:05:14 +0200

    edit PageTitle

--- app/src/components/installations/PageTitle.tsx
+++ app/src/components/installations/PageTitle.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import HeaderMenu from '../metierscomponents/HeaderMenu'; // <- remonte d'un dossier
 
 const PageTitle: React.FC = () => {
   return (
     <div className="fr-grid-row fr-grid-row--between fr-mb-3w">
       <div className="fr-col-12 fr-col-md-6">
         <h1 className="fr-h2">Je valorise mon lycée</h1>
-      </div>
-      <div className="fr-col-12 fr-col-md-6 fr-text--right">
-        <HeaderMenu />
       </div>
     </div>
   );
