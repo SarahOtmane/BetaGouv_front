0306dff86dda6ecb615e4a254fe5f31cb107a3a5
amiramiour <amiouramirtahar@gmail.com>
2025-07-01 14:43:53 +0200

    add ActionsProgrammes

---
+++ app/src/components/actions/ActionsProgrammes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ActionsProgrammes: React.FC = () => {
+  return (
+    <div className="fr-mb-4w">
+      <h4 className="fr-text--md fr-mb-2w">Les actions programmées par le Lycée :</h4>
+      <ul className="fr-pl-2w">
+        <li>Journée portes ouvertes : le 3 mars 2024</li>
+        <li>Job dating commerce : le 12 mai 2024</li>
+        <li>Réunion d'information : Sécurité : 25 Avril 2024</li>
+        <li>Forum Hôtellerie/Restauration : 15 Mars 2024</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ActionsProgrammes;
