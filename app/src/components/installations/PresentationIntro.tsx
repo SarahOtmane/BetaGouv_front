53a2e280aa5da3dd88fdddce4b9099fa286c48a7
amiramiour <amiouramirtahar@gmail.com>
2025-07-01 14:31:26 +0200

    add Présenter nos plateaux techniques part

---
+++ app/src/components/installations/PresentationIntro.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PresentationIntro: React.FC = () => {
+  return (
+    <section className="fr-mb-5w">
+      <h2 className="fr-h4">Présenter nos plateaux techniques</h2>
+      <p className="fr-text--sm">
+        Nous disposons d'un atelier Chaudronnerie, d'un véritable entrepôt logistique ainsi que de 3 cuisines d'application.
+      </p>
+    </section>
+  );
+};
+
+export default PresentationIntro;
